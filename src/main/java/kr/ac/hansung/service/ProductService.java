0cf2441642416fc21d7594d70e19a4c1dd5b8963
ss-ngm-n <sungmin10057@naver.com>
2026-05-31 17:31:24 +0900

    feat: 상품 목록 페이징 및 키워드 검색 기능 구현

--- src/main/java/kr/ac/hansung/service/ProductService.java
+++ src/main/java/kr/ac/hansung/service/ProductService.java
@@ -4,6 +4,8 @@ import kr.ac.hansung.dto.ProductDto;
 import kr.ac.hansung.entity.Product;
 import kr.ac.hansung.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,5 +40,15 @@ public class ProductService {
     @Transactional
     public void deleteById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    // 전체 목록 페이징
+    public Page<Product> getProducts(Pageable pageable){
+        return productRepository.findAll(pageable);
+    }
+
+    // 검색 + 페이징
+    public Page<Product> searchProducts(String keyword, Pageable pageable){
+        return productRepository.findByNameContaining(keyword, pageable);
     }
 }
